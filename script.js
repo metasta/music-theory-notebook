236913805a6376862fb3c200db525d2793e07674
Matsumoto Yoichiro <metasta@gmail.com>
2018-04-22 23:23:18 +0900

    「和声」と表紙追加、目次改良

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function maketoc(){
+function maketoc2(){
  var id_toc = 'table-of-contents';
  if(null == document.getElementById(id_toc)){ return 1; }
 
@@ -25,4 +25,51 @@ function maketoc(){
   ol.appendChild(li);
  }
  document.getElementById('table-of-contents').appendChild(ol);
+
 };
+
+function maketoc(){
+ if (null == document.getElementById('table-of-contents')){ return 1; }
+
+ var ol = document.createElement('ol');
+ var hlist = document.querySelectorAll('h2, h3');
+ var count_h2 = 0;
+ var count_h3 = 0;
+ var prefix = "";
+
+ for (var i = 0; i < hlist.length; i++){
+  if (hlist[i].tagName == 'H2') {
+   count_h2++;
+   count_h3 = 0;
+   prefix = "§" + count_h2;
+  }
+  else if (hlist[i].tagName == 'H3') {
+   count_h3++;
+   prefix = count_h2 + "." + count_h3;
+  }
+  var id = encodeURIComponent(prefix + "_" + (hlist[i].hasAttribute('title') ? hlist[i].getAttribute('title') : hlist[i].innerHTML) );
+
+  var a = document.createElement('a');
+  a.setAttribute('href', '#' + id);
+  a.innerHTML = prefix + " " + hlist[i].innerHTML;
+
+  var li = document.createElement('li');
+  li.appendChild(a);
+  li.classList.add(hlist[i].tagName);
+
+  ol.appendChild(li);
+
+  hlist[i].innerHTML = prefix + " " + hlist[i].innerHTML;
+  hlist[i].id = id;
+ }
+ document.getElementById('table-of-contents').appendChild(ol);
+}
+
+function renderabc(){
+ var score = document.getElementsByClassName("score");
+ if (score) {
+  for( var i in score ){
+   ABCJS.renderAbc( score[i], score[i].innerHTML,{staffwidth:300} );
+  }
+ }
+}
